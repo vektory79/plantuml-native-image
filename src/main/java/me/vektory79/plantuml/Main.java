f0c45c957a3320b16973e01409050c357b4e5d3c
vektory79 <vektory79@gmail.com>
2019-07-13 22:08:09 +0300

    Successfully compalable but not runnable

--- src/main/java/me/vektory79/plantuml/Main.java
+++ src/main/java/me/vektory79/plantuml/Main.java
@@ -1,13 +1,19 @@
 package me.vektory79.plantuml;
 
-import me.vektory79.plantuml.stubs.XToolkitStub;
 import net.sourceforge.plantuml.Run;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        XToolkitStub.XToolkitStub_Thread_Recalc.start();
-        Run.main(args);
+        String currentDir = System.getProperty("user.dir");
+        System.out.println("user.dir=" + currentDir);
+        System.setProperty("java.awt.headless", "true");
+
+        if (args.length == 0) {
+            Run.main(new String[]{"/media/data/java/plantuml-graal/examples/example1.txt"});
+        } else {
+            Run.main(args);
+        }
     }
 }
